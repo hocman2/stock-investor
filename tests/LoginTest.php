12afa8f314573c7608fde5b91029b03a9db315a9
Matthis <matthishoc@gmail.com>
2023-07-07 14:18:06 +0200

    login controller

---
+++ tests/LoginTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoginTest extends WebTestCase
+{
+    public function testLogin(): void
+    {
+        $client = static::createClient();
+
+        // Try to login with this wrong credentials/data
+        $crawler = $client->request('GET', '/login', ["username" => "azerty"]);
+        $this->assertSame(Response::HTTP_INTERNAL_SERVER_ERROR, $client->getResponse()->getStatusCode());
+        $crawler = $client->request('GET', '/login', ["username" => "azerty", "password" => "123"]);
+        $this->assertSame(Response::HTTP_BAD_REQUEST, $client->getResponse()->getStatusCode());
+    
+        
+    }
+}
